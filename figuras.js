4aa24fbe619062b93353bfc0edbb97ee4bdfc87b
FrancoMcab <f.cabello2807@gmail.com>
2021-08-23 17:40:40 -0300

    Actualizacion interfaz y metodo para sacar altura en triangulos isosceles

--- figuras.js
+++ figuras.js
@@ -16,9 +16,20 @@ console.groupEnd();
 console.group("Triangulo");
 
 function perimetroTriangulo(lado1,lado2,base){
-    return lado1+lado2+base;
+    const perimetro =lado1+lado2+base;
+    return perimetro;
 }
-    
+function alturaIsosceles(lado1,lado2,base){
+   if(lado1 == lado2 && lado1 !=base ){
+    console.log("Los lados 1 y 2 son iguales, este triangulo es isosceles");
+    const altura = Math.sqrt((lado1**2)-((base**2)/4));
+    console.log("La altura de este triangulo es: "+altura);
+    return altura;
+   } else{
+      return console.log("Este triangulo no es isosceles");
+   }
+   
+}
 function areaTriangulo (base, altura){
     return (base*altura)/2;
 }
@@ -47,14 +58,35 @@ console.groupEnd();
 //Aca interactiamos con HTML
 
 function calcularPerimetroCuadrado(){
-    const input= document.getElementById("input-Cuadrado");
+    const input= document.getElementById("inputCuadrado");
     const value= input.value;
     const perimeto= perimetoCuadrado(value);
     alert(perimeto);
 }
 function calcularAreaCuadrado(){
-    const input= document.getElementById("input-Cuadrado");
+    const input= document.getElementById("inputCuadrado");
     const value= input.value;
     const area= areaCuadrado(value);
     alert(area);
+}
+function calcularPerimetroTriangulo(){
+    const input1= document.getElementById("inputTrianguloL1");
+    const value1= parseInt(input1.value); 
+    const input2= document.getElementById("inputTrianguloL2");
+    const value2= parseInt(input2.value); 
+    const inputB= document.getElementById("inputTrianguloB");
+    const valueB= parseInt(inputB.value); 
+    const perimeto= perimetroTriangulo(value1,value2,valueB);
+    alert(perimeto);
+}
+function calcularAreaTriangulo(){
+    const input1= document.getElementById("inputTrianguloL1");
+    const value1= parseInt(input1.value); 
+    const input2= document.getElementById("inputTrianguloL2");
+    const value2= parseInt(input2.value); 
+    const inputB= document.getElementById("inputTrianguloB");
+    const valueB= parseInt(inputB.value); 
+    const altura = alturaIsosceles(value1,value2,valueB);
+    const perimeto= areaTriangulo(valueB,altura);
+    alert(perimeto);
 }
